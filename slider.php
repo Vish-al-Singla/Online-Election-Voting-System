f405af5444fdbb2974f31a19a19a7257b9b1bf53
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-07-10 14:39:53 +0530

    Slider: Added Image Slider

---
+++ slider.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/slider.css">
+</head>
+<body>
+
+</body>
+</html>
+<div id="slider">
+	<figure>
+<img src="img/India-Elections-2019.jpg">
+<img src="img/voting1.png">
+<img src="img/voting4.jpg">
+<img src="img/voting3.jpg">
+<img src="img/voting2.jpg">
+    </figure>
+</div>
