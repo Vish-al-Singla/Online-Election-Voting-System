f22fdd02d411b561caad683f971ce92ae664ad19
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-07-10 14:37:38 +0530

    Update: Added Footer and Header

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+<footer class="footer">
+	<hr/>
+&copy2021. Developed by Vishal Singla, Summer Gagneja, Ujjwal Khanna.
+<br/>
+Online Voting System.
+<br/>
+Minor Project 2021. Guru Nanak Dev Engineering College, Ludhiana
+<br/>
+Contact: <a href="tel:8872425416">
+	+91-8872425416</a> | Email: <a href="mailto:s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com">s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com</a>
+
+</footer>
