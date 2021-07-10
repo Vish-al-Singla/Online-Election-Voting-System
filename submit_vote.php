247a287f0d8999b363bf534ae27e40a8deac0dd5
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-07-10 15:03:25 +0530

    Feat: Vote Submission Pages

---
+++ submit_vote.php
@@ -0,0 +1,11 @@
+<?php
+	include("admin/dbcon.php");
+	session_start();
+	session_destroy();
+		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[pm_id]', '$_SESSION[voters_id]')") or die($conn->error);
+		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[cm_id]', '$_SESSION[voters_id]')") or die($conn->error);
+		$conn->query("INSERT INTO `votes` VALUES('', '$_SESSION[mla_id]', '$_SESSION[voters_id]')") or die($conn->error);
+		$conn->query("UPDATE `voters` SET `status` = 'Voted' WHERE `voters_id` = '$_SESSION[voters_id]'") or die($conn->error);
+		header("location:index.php");
+		
+?> 
