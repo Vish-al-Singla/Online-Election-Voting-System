4d1be23690b788bac292552992cb6ca0e6fda30f
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-07-10 14:34:22 +0530

    Feat: Added HomePage

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include ('head.php');?>
+
+<body>
+	<?php include ('view_banner.php');?>
+
+    
+    <?php include 'slider.php';?>
+
+    <?php    
+        include ('footer.php');
+        ?>
+
+   </body>
+</html>
+
